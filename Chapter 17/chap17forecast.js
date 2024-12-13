dc97713ad7b4dd4a3069431510da407d5e565a3d
xyzBENTLEY <pitts.evangelis867@gmail.com>
2024-12-13 02:58:36 -0600

    Add files via upload

---
+++ Chapter 17/chap17forecast.js
@@ -0,0 +1,38 @@
+// //Listing 17.1
+// for (var i = 10; i > 0; i--) {
+//     alert(i);
+// }
+// alert("Blast Off!");
+
+// //Listing 17.2
+// var myFriends = ["Agatha", "Agnes", "Jermaine", "Jack"];
+// for (var i = 0; i < myFriends.length; i++){
+//  alert(myFriends[i] + " is my friend.");
+// }
+
+// //Listing 17.3
+// alert(Math.random());
+
+// //Listing 17.4
+// var myFriends = ["Agatha", "Agnes", "Jermaine", "Jack"];
+// var randomFriend = Math.floor(Math.random() * myFriends.length);
+// alert(myFriends[randomFriend]);
+
+//Listing 17.5
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+var weather = ["Sunny", "Partly Sunny", "Partly Cloudy", "Cloudy", "Raining", "Snowing", "Thunderstorm", "Foggy"];
+
+var maxTemp = 100;
+var minTemp = 0;
+generateWeather();
+
+function generateWeather() {
+    for (var i = 0; i < days.length; i++) {
+        var weatherToday = weather[Math.floor(Math.random() * weather.length)];
+        var tempToday = Math.floor(Math.random() * (maxTemp - minTemp) + minTemp);
+        
+        document.getElementById("5DayWeather").innerHTML += "<div id='" + days[i] + "' class='" +
+            weatherToday + "'><b>Forecast for " + days[i] + ":</b><br><br>" + weatherToday + " and " + 
+            tempToday + " degrees.</div>";
+    }
+}
